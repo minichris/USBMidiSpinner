738ce752f37aefa0b4ff0087d899b676613e163b
Christopher Lee <minichris502@gmail.com>
2024-08-25 02:06:52 +0100

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,16 @@
 #include <Bounce2.h>
 #include <ResponsiveAnalogRead.h>
 
+const int HallAMax = 529;
+const int HallAMin = 514;
+const int HallBMax = 533;
+const int HallBMin = 524;
+const int HallAvMax = 1057;
+const int HallAvMin = 1035;
+
 // USB MIDI object
 Adafruit_USBD_MIDI usbMidi;
 MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usbMidi, MIDI);
-
-// Example of handling midi input to the device
-void handle_midi_note_on(byte channel, byte note, byte velocity) {
-  Serial.println("Got note on!");
-}
 
 void setup() {
   Serial.begin(115200);
@@ -25,33 +27,100 @@ void setup() {
   usbMidi.begin();
   MIDI.begin();
 
-  // Turn midi thru off
-  MIDI.turnThruOff();
-
-  // Set the MIDI note on handling function
-  MIDI.setHandleNoteOn(handle_midi_note_on);
-
+  pinMode(26, INPUT);
+  pinMode(27, INPUT);
+  pinMode(22, OUTPUT);
+  digitalWrite(22, 1);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, 1);
 }
 
-auto pastLEDState = true;
-void loop() {
-  MIDI.read();
+int MapToMidi(int input, int inputMin, int inputMax) {
+  return min(127, max(0, map(input, inputMin, inputMax, 0, 127)));
+}
+
+int LastSentMidiA = -255;
+int LastSentMidiB = -255;
+int MidiA, MidiB;
+
+
+
+bool WaitingForDrop = false;
+bool BReachedMax = false;
+void Mode1(){
+	int analogA = analogRead(A0);
+	int analogB = analogRead(A1);
   
-  Serial.println("Hello from da loop!");
+    if(!WaitingForDrop){
+		MidiA = MapToMidi(analogA, HallAMin, HallAMax);
+		if (MidiA != LastSentMidiA) {
+			MIDI.send(midi::ControlChange, 1, MidiA, 12U);
+			LastSentMidiA = MidiA;
+				if(LastSentMidiA > 120){
+					WaitingForDrop = true;
+				}
+		}
+	}
+	else{
+		MidiB = MapToMidi(analogB, HallBMin, HallBMax);
+		if (MidiB != LastSentMidiB) {
+			if(MidiB > 120){
+				BReachedMax = true;
+			}
+			if(BReachedMax == true){
+				MIDI.send(midi::ControlChange, 1, MidiB, 12U);
+				LastSentMidiB = MidiB;
+				if(LastSentMidiB < 5){
+				BReachedMax = false;
+				WaitingForDrop = false;
+				}
+			}		
+		}
+	}
+
+	Serial.print("\nAnalog A value: ");
+	Serial.print(analogA);
+	Serial.print("\nAnalog B value: ");
+	Serial.print(analogB);
+}
+
+void Mode0(){
+  int analogA = analogRead(A0);
+  int analogB = analogRead(A1);
+
+  MidiA = MapToMidi(analogA, HallAMin, HallAMax);
+  if (MidiA != LastSentMidiA) {
+    MIDI.send(midi::ControlChange, 1, MidiA, 12U);
+    LastSentMidiA = MidiA;
+  }
+
+  MidiB = MapToMidi(analogB, HallBMin, HallBMax);
+  if (MidiB != LastSentMidiB) {
+    MIDI.send(midi::ControlChange, 1, MidiB, 14U);
+    LastSentMidiB = MidiB;
+  }
+
+  Serial.print("\nAnalog A value: ");
+  Serial.print(analogA);
+  Serial.print("\nAnalog B value: ");
+  Serial.print(analogB);
+}
+
+auto pastLEDState = true;
+int Mode = 1;
+void loop() {
+  //MIDI.read();
 
   // Blink the LED
   digitalWrite(LED_BUILTIN, static_cast<int>(pastLEDState));
-
-  // Send a midi note
-  if(pastLEDState){
-    MIDI.sendNoteOn(60, 127, 1);
-  } else {
-    MIDI.sendNoteOff(60, 0, 1);
+  
+  switch(Mode){
+	case 0:
+	Mode0();
+	break;
+	case 1:
+	Mode1();
+	break;
   }
 
-  pastLEDState = !pastLEDState;
-
-  delay(500);
 }
